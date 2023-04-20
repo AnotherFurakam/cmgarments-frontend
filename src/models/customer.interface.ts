67473554d048bf6ce2aa58d6bf24f9208d5984cb
Wilmer <wilmer.r2004@gmail.com>
2023-04-20 01:56:08 -0500

    tabla salida

---
+++ src/models/customer.interface.ts
@@ -0,0 +1,10 @@
+export interface ICustomer {
+  id_customer?: string;
+  names: string;
+  first_lastname: string;
+  second_lastname: string;
+  dni: string;
+  phone_number: string;
+  email: string;
+  create_at?: string;
+}
